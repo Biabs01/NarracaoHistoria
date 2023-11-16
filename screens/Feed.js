6464175991f4ca16f60d48ec4220a6d266bacc5c
Biabs01 <108082691+Biabs01@users.noreply.github.com>
2023-11-16 15:47:33 -0300

    new file:   assets/cloud.png new file:   assets/google_icon.png new file:   assets/logo.png new file:   assets/profile_img.png new file:   assets/story_image_2.png new file:   assets/story_image_3.png new file:   assets/story_image_4.png new file:   assets/story_image_5.png modified:   babel.config.js modified:   screens/Feed.js new file:   screens/StoryCard.js new file:   screens/temp_stories.json

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,16 +1,55 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, Platform, StatusBar, Image} from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import StoryCard from './StoryCard';
+import AppLoading from 'expo-app-loading';
+import { FlatList } from 'react-native-gesture-handler';
+
+let customFonts = {"Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")};
+
+let stories = require("./temp_stories.json");
 
 export default class Feed extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            fontLoaded: false
+        }
+    }
+
+    async _loadFontsAsync(){
+        await Font.loadAsync(customFonts);
+        this.setState({ fontLoaded: true });
+    }
+
+    componentDidMount(){
+        this._loadFontsAsync();
+    }
+
+    renderItem = ({item: story}) => {
+        return <StoryCard story={story}/>;
+    };
+
+    keyExtractor = (item, index) => index.toString();
+
     render(){
-        return(
-            <View style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-                <Text>Feeds</Text>
-            </View>
-        )
+        if (!this.state.fontLoaded){
+            return <AppLoading/>
+        } else {
+            return(
+                <View style={styles.container}>
+                    <SafeAreaView style={styles.droidSafeArea}/>
+                    <View style={styles.appTitle}>
+                        <View style={styles.appIcon}>
+                            <Image
+                                source={require('../assets/logo.png')}
+                                style={styles.iconImage}
+                            ></Image>
+                        </View>
+
+                    </View>
+                </View>
+            );
+        } 
     }
 }
