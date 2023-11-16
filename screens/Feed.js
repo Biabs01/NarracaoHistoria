21da5f69031072132d76580834170c382a183e2f
Biabs01 <108082691+Biabs01@users.noreply.github.com>
2023-11-16 16:06:14 -0300

    modified:   package-lock.json modified:   screens/Feed.js modified:   yarn.lock

--- screens/Feed.js
+++ screens/Feed.js
@@ -4,6 +4,7 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import StoryCard from './StoryCard';
 import AppLoading from 'expo-app-loading';
 import { FlatList } from 'react-native-gesture-handler';
+import * as Font from 'expo-font';
 
 let customFonts = {"Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")};
 
@@ -46,10 +47,56 @@ export default class Feed extends Component{
                                 style={styles.iconImage}
                             ></Image>
                         </View>
-
+                        <View style={styles.appTitleTextContainer}>
+                            <Text style={styles.appTitleText}>App Narração de Histórias</Text>
+                        </View>
+                        <View style={styles.cardContainer}>
+                            <FlatList
+                                keyExtractor={this.keyExtractor}
+                                data={stories}
+                                renderItem={this.renderItem}
+                            />
+                        </View>
+                        <View style={{flex: 0.08}}/>
                     </View>
                 </View>
             );
         } 
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#15193c'
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : RFValue(35)
+    },
+    appIcon: {
+        flex: 0.3,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    appTitle: {
+        flex: 0.7,
+        flexDirection: 'row'
+    },
+    iconImage: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain'
+    },
+    appTitleTextContainer: {
+        flex: 0.7,
+        justifyContent: 'center'
+    },
+    appTitleText: {
+        color: 'white',
+        fontSize: RFValue(28),
+        fontFamily: 'Bubblegum-Sans'
+    },
+    cardContainer: {
+        flex: 0.85
+    }
+});
